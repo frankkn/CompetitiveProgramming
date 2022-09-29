10c7cb1011103136faa5d329533eb7157e15a29c
Frank <frank840629@gmail.com>
2022-09-29 17:05:49 +0800

    Add a solution with lowbit. (Fastest sol. so far.)

---
+++ Unit2_回溯與剪枝/Class/八皇后問題/lowbit.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int MASK;
+int ans;
+void dfs(int M, int L, int R);
+
+int lowbit(int x) { return x & -x; }
+void dfs(int M, int L, int R) {
+  if (M == MASK) {
+    ++ans;
+    return;
+  }
+  int Legal = MASK & ~(M | L | R);
+  for (int num = 0; Legal; Legal ^= num) {
+    num = lowbit(Legal);
+    dfs(M | num, (L | num) << 1, (R | num) >> 1);
+  }
+}
+
+int main() {
+  int n = 0;
+  cin >> n;
+  MASK = (1 << n) - 1;
+  dfs(0, 0, 0);
+  cout << ans << endl;
+  return 0;
+}
