6a93b365d2f93a4fcea9cd50e096836593265689
Frank <frank840629@gmail.com>
2022-10-02 18:32:22 +0800

    Use set to prune duplicate elements.

--- Unit2_回溯與剪枝/HW/hw2_2.cpp
+++ Unit2_回溯與剪枝/HW/hw2_2.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <set>
+#include <string>
 using namespace std;
 
 int target, sz;
-bool flag;
-int num[12], ans[12];
+int num[13];
+set<vector<int>> res;
+vector<int> ans;
 
-void dfs(int idx, int m, int sum)
+void print()
 {
-  int last = -1;
+  int n = res.size();
+  for(auto it = res.rbegin(); it != res.rend() ; ++it)
+  {
+    int m = (*it).size();
+    for(int j = 0; j != m; ++j)
+    {
+      cout << (*it)[j] << "+\n"[j == m-1];
+    }
+  }
+}
+
+void dfs(int idx, int sum)
+{
   if(sum > target)	return;
   if(sum == target)
   {
-  	flag = false;
-    for(int i = 0; i < m; ++i)
-    {
-      cout << ans[i] << "+\n"[i == m-1];
-    }
+	res.insert(ans);
     return;
   }
   for(int i = idx; i < sz; ++i)
   {
   	if(sum + num[i] > target)	continue;
-    if(num[i] != last)
-    {
-      ans[m] = num[i];
-      last = ans[m];
-      dfs(i+1, m+1, sum+num[i]);
-    }
+    ans.push_back(num[i]);
+    dfs(i+1, sum+num[i]);
+    ans.pop_back();
   }
 }
 
@@ -35,18 +44,17 @@ int main()
 {
   while( cin >> target >> sz && (target || sz) )
   {
-    flag = true;
     memset(num, 0, sizeof(num));
-    for(int i = 0; i < sz; ++i)
-    {
-    	cin >> num[i];
+    for(int i = 0; i < sz; ++i) 
+    { 
+      cin >> num[i]; 
     }
     cout << "Sums of " << target << ":\n";
-    dfs(0,0,0);
-    if(flag)
-    {
-    	cout << "NONE\n";
-    }
+    dfs(0,0);
+    print();
+    if(res.empty()) { cout << "NONE\n"; }
+    ans.clear();
+    res.clear();
   }
   return 0;
 }
