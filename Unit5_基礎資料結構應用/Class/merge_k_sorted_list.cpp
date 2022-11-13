926c1d40a0718ad72c21cc66fafd27d0dddb1b90
Frank <frank840629@gmail.com>
2022-11-13 19:57:14 +0800

    Update a simple solution

--- Unit5_基礎資料結構應用/Class/merge_k_sorted_list.cpp
+++ Unit5_基礎資料結構應用/Class/merge_k_sorted_list.cpp
@@ -10,30 +10,67 @@ struct ListNode {
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-ListNode* mergeKLists(vector<ListNode*>& lists);
-
-struct CMP 
-{
-  bool operator()(ListNode *a, ListNode *b) const { return a->val > b->val; }
+class Solution {
+public:
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+    // top is the smallest
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    
+    for(int i = 0; i < lists.size(); ++i)
+    {
+        ListNode* head = lists[i];
+        while(head != nullptr)
+        {
+            PQ.emplace(head->val);
+            head = head->next;
+        }
+    }
+    
+    ListNode* start = nullptr;
+    ListNode* end = nullptr;
+    while(!PQ.empty())
+    {
+        auto node = PQ.top(); PQ.pop();
+        if(start == nullptr)
+        {
+            start = new ListNode(node);
+            end = start;
+        }
+        else
+        {
+            end->next = new ListNode(node);
+            end = end->next;
+        }
+    }
+    return start;
+  }
 };
 
-priority_queue<ListNode *, vector<ListNode *>, CMP> PQ;
-void initPQ(vector<ListNode *> &lists) {
-  decltype(PQ)().swap(PQ);
-  for (auto nd : lists)
-    if (nd) PQ.emplace(nd);
-}
 
-ListNode *mergeKLists(vector<ListNode *> &lists) {
-  initPQ(lists);
-  ListNode *root = nullptr;
-  ListNode **cur = &root;
-  while (PQ.size()) {
-    auto nd = PQ.top();
-    PQ.pop();
-    *cur = nd;
-    cur = &nd->next;
-    if (*cur) PQ.emplace(*cur);
-  }
-  return root;
-}
+// ListNode* mergeKLists(vector<ListNode*>& lists);
+
+// struct CMP 
+// {
+//   bool operator()(ListNode *a, ListNode *b) const { return a->val > b->val; }
+// };
+
+// priority_queue<ListNode *, vector<ListNode *>, CMP> PQ;
+// void initPQ(vector<ListNode *> &lists) {
+//   decltype(PQ)().swap(PQ);
+//   for (auto nd : lists)
+//     if (nd) PQ.emplace(nd);
+// }
+
+// ListNode *mergeKLists(vector<ListNode *> &lists) {
+//   initPQ(lists);
+//   ListNode *root = nullptr;
+//   ListNode **cur = &root;
+//   while (PQ.size()) {
+//     auto nd = PQ.top();
+//     PQ.pop();
+//     *cur = nd;
+//     cur = &nd->next;
+//     if (*cur) PQ.emplace(*cur);
+//   }
+//   return root;
+// }
