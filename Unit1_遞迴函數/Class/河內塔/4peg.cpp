3ec3f03c6854b3b536d1cab62c62a3864bc45631
Frank <frank840629@gmail.com>
2022-11-21 14:00:12 +0800

    Add 4 pegs program

---
+++ Unit1_遞迴函數/Class/河內塔/4peg.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int step = 0;
+
+void hanoi(int n, char from, char aux1, char aux2, char to) // A:from B:aux1 C:aux2 D:to
+{
+  if(n == 0)
+  {
+    return;
+  }
+  step++;
+  hanoi(n-2, from, aux1, aux2, to); 
+  printf("Move ring %d from %c to %c\n", n-1, from, aux2);
+  printf("Move ring %d from %c to %c\n", n, from, to);
+  printf("Move ring %d from %c to %c\n", n-1, aux2, to);
+  hanoi(n-2, aux1, to, from, aux2);
+  return;
+}
+
+int main()
+{
+  int n = 4; // # of disks
+  hanoi(n, 'A', 'B', 'C', 'D');
+  cout << step;
+  return 0;
+}
