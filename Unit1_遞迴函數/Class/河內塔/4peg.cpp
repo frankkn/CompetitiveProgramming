22be161048f82a15f3e3d1edcf1dd258f4e72e7d
Frank <frank840629@gmail.com>
2022-11-21 16:01:03 +0800

    Debug 4 peg

--- Unit1_遞迴函數/Class/河內塔/4peg.cpp
+++ Unit1_遞迴函數/Class/河內塔/4peg.cpp
@@ -16,17 +16,17 @@ void hanoi(int n, char from, char aux1, char aux2, char to) // A:from B:aux1 C:a
     return;
   }
   step += 3;
-  hanoi(n-2, from, aux1, aux2, to); 
+  hanoi(n-2, from, aux2, to, aux1); 
   printf("Move ring %d from %c to %c\n", n-1, from, aux2);
   printf("Move ring %d from %c to %c\n", n, from, to);
   printf("Move ring %d from %c to %c\n", n-1, aux2, to);
-  hanoi(n-2, aux1, to, from, aux2);
+  hanoi(n-2, aux1, from, aux2, to);
   return;
 }
 
 int main()
 {
-  int n = 5; // # of disks
+  int n = 3; // # of disks
   hanoi(n, 'A', 'B', 'C', 'D');
   cout << step;
   return 0;
