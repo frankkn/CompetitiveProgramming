3ef9b279668a5ce6e723064faa58a4fb6bae599d
Frank <frank840629@gmail.com>
2022-11-21 14:22:01 +0800

    Update 4 peg solution with showing Steps

--- Unit1_遞迴函數/Class/河內塔/4peg.cpp
+++ Unit1_遞迴函數/Class/河內塔/4peg.cpp
@@ -9,7 +9,13 @@ void hanoi(int n, char from, char aux1, char aux2, char to) // A:from B:aux1 C:a
   {
     return;
   }
-  step++;
+  if(n == 1) 
+  {
+    step += 1;
+    printf("Move ring %d from %c to %c\n", n, from, to);
+    return;
+  }
+  step += 3;
   hanoi(n-2, from, aux1, aux2, to); 
   printf("Move ring %d from %c to %c\n", n-1, from, aux2);
   printf("Move ring %d from %c to %c\n", n, from, to);
@@ -20,7 +26,7 @@ void hanoi(int n, char from, char aux1, char aux2, char to) // A:from B:aux1 C:a
 
 int main()
 {
-  int n = 4; // # of disks
+  int n = 5; // # of disks
   hanoi(n, 'A', 'B', 'C', 'D');
   cout << step;
   return 0;
