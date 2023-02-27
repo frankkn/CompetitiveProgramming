57d80af25f9c320c4089b3ce2baa04c5b98f3614
Frank <frank840629@gmail.com>
2023-02-27 22:31:29 +0800

    add BIT

---
+++ Unit16_BinaryIndexTree/bit.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class BIT {
+ int n;
+ vector<long long> bit;
+public:
+ void init(int _n) {
+ n = _n;
+ bit.resize(n);
+ for (auto &b : bit) b = 0;
+ }
+
+ long long query(int x) const {
+ long long sum = 0;
+ for (; x; x -= lowbit(x))
+ sum += bit[x];
+ return sum;
+ }
+ 
+ void modify(int x, int val) {
+ for (; x <= n; x += lowbit(x))
+ bit[x] += val;
+ }
+};
